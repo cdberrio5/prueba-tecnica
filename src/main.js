ddeb1994ef6662352221ca8d0c3d969949ed0b05
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-25 21:40:12 -0500

    Project creation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import './styles.scss'
+
+Vue.use(ElementUI, { locale })
 
 createApp(App).mount('#app')
