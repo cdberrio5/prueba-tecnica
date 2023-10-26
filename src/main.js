2051eca2c2eeb8979ab2fd8a576dd43ac7f2677f
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-26 14:25:16 -0500

    config tailwind

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
-import './styles.scss'
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+import 'element-ui/lib/theme-chalk/index.css';
+import Vue, { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+import './styles.scss';
 
 Vue.use(ElementUI, { locale })
 
-createApp(App).mount('#app')
+createApp(App, store, router).mount('#app')
