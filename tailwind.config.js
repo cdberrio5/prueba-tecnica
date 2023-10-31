547edb3e5b8a9376ae128512bc1bbed1f0ce41a2
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-30 19:06:26 -0500

    all configuration for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{html,vue,js}",
+  ],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
