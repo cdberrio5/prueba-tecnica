e2c1e34471cc36fe67b6545e980fa1c2127730d4
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-30 19:08:26 -0500

    Company structure and menu

---
+++ src/components/Card/CardPage.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="relative bg-white rounded-md overflow-hidden shadow-lg p-6 card">
+    <div class="w-[170px] h-[170px] bg-slate-200 rounded-[185px] logo">
+        <div class="text-indigo-900 text-[80px]">AC</div>
+    </div>
+
+    <h2 class="text-xl font-bold text-indigo-900">A.C Telecom</h2>
+    <p class="mb-2">@ac-telecom</p>
+
+    <p class="text-center mb-2 text-slate-400 text-[13px] font-semibold leading-tight tracking-wide">Startup - tecnología</p>
+
+    <div class="text-icon mb-2 w-28">
+        <div class="w-2 h-2 p-1 bg-lime-400 rounded-3xl"></div>
+        <p class="text-lime-400 text-xs leading-[18px] tracking-wide">Empresa activa</p>
+    </div>
+
+    <div class="text-icon mb-4 w-44">
+        <i class="fa-solid fa-location-dot"></i>
+        <p class="text-center text-stone-500 text-xs font-normal leading-[18px] tracking-wide">Floridablanca - Colombia</p>
+    </div>
+
+    <div class="flex items-center w-44 container-rating mb-4">
+        <div class="rating">
+            <i class="fas fa-star text-yellow-400"></i>
+            <i class="fas fa-star text-yellow-400"></i>
+            <i class="fas fa-star text-yellow-400"></i>
+            <i class="fas fa-star text-yellow-400"></i>
+            <i class="fas fa-star text-yellow-100"></i>
+        </div>
+
+        <p class="text-center text-stone-500 text-xs font-normal leading-[18px] tracking-wide">(10 Reviews)</p>
+    </div>
+
+    <div class="flex space-x-2 mb-4 text-3xl">
+      <i class="fab fa-facebook-square text-blue-800"></i>
+      <i class="fab fa-instagram text-pink-800"></i>
+      <i class="fab fa-twitter text-blue-800"></i>
+      <i class="fab fa-linkedin text-blue-800"></i>
+    </div>
+
+    <div class="text-icon mb-4 w-60 text-pink-600">
+        <i class="fa-solid fa-paperclip"></i>   
+        <p class="text-xs font-normal leading-[18px] tracking-wide">https://www.actelecom.net/telecom</p>
+    </div>
+
+    <img src="@/assets/video.png" alt="QR Code" class="w-full mb-4 rounded-md">
+
+    <img src="@/assets/QR.jpg" alt="QR Code" class="w-full mb-4 rounded-md">
+
+    <button class="w-full mb-4 bg-pink-600 text-white px-4 py-2 rounded-md focus:outline-none focus:shadow-outline-blue justify-between items-center">
+        <i class="fa-solid fa-share"></i>
+        Compartir
+    </button>
+
+    <button class="w-full text-stone-500 flex gap-2.5 rounded-l-md inline-flex border-2 border-slate-400 mb-4">
+        <div class="w-10 bg-pink-600 text-white px-4 py-2 rounded-l-md flex justify-center align-center">
+            <i class="fa-solid fa-download"></i>
+        </div>
+        <div class="w-60"> 
+            Descargar Resumen
+        </div>
+    </button>
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-users"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Talentos</p>
+        </div>
+
+        <p class="text-center ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+
+    <hr style="border-bottom: 1px solid rgba(200, 200, 200, 0.4); border-radius: 10px;" class="mb-4 w-full" />
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-eye"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Vistas del perfil</p>
+        </div>
+
+        <p class="text-center ml-auto ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+
+    <hr style="border-bottom: 1px solid rgba(200, 200, 200, 0.4); border-radius: 10px;" class="mb-4 w-full" />
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-video"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Pitches del perfil</p>
+        </div>
+
+        <p class="text-center ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+
+    <hr style="border-bottom: 1px solid rgba(200, 200, 200, 0.4); border-radius: 10px;" class="mb-4 w-full" />
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-person"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Exhibiciones</p>
+        </div>
+
+        <p class="text-center ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+
+    <hr style="border-bottom: 1px solid rgba(200, 200, 200, 0.4); border-radius: 10px;" class="mb-4 w-full" />
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-clipboard-user"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Vacantes</p>
+        </div>
+
+        <p class="text-center ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+
+    <hr style="border-bottom: 1px solid rgba(200, 200, 200, 0.4); border-radius: 10px;" class="mb-4 w-full" />
+
+    <div class="w-full flex items-center mb-4 text-lg uppercase">
+        <div class="flex justify-start items-center">
+            <i class="fa-solid fa-images"></i>
+            <p class="ml-1 text-center text-stone-500 leading-[18px] tracking-wide">Contenidos</p>
+        </div>
+
+        <p class="text-center ml-auto text-stone-500 leading-[18px] tracking-wide">20</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyCard',
+};
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
+
+    .card {
+        max-width: 350px;
+        top: -90px;
+        margin-right: auto;
+        margin-left: 5%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Poppins', sans-serif;
+        font-weight: bold;
+        letter-spacing: -20px;
+    }
+
+    p {
+        font-family: 'Inter', sans-serif;
+    }
+
+    .text-icon {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .container-rating {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
---
+++ src/views/Company/CompanyPage.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+      <div class="h-40 bg-cover bg-center" :style="{ backgroundImage: 'url(' + require('@/assets/cielo-estrellado.jpg') + ')' }"></div>
+
+      <div class="container">
+        <CardPage />
+
+        <div class="w-full flex bg-slate-200 flex-col">
+          <div class="w-full flex bg-white h-16 justify-center items-center uppercase p-10">
+            <p class="text-center text-slate-400 text-[13px] font-semibold leading-tight tracking-wide">Match con la cultura</p>
+
+            <div class="ml-4 flex justify-center items-center ">
+              <p class="text-center text-lime-400 text-xs font-normal leading-[18px] tracking-wide">80%</p>
+
+              <div class="w-[276px] h-3.5 relative bg-slate-200 rounded-[30px] ml-2">
+                <div class="w-[207px] h-3.5 relative bg-lime-400 rounded-[30px]"></div>
+              </div>
+            </div>
+
+            <div class="ml-auto h-10 px-3 py-2 bg-pink-600 rounded-md justify-center items-center gap-2.5 inline-flex">
+              <p class="text-white text-[13px] font-semibold leading-tight tracking-wide">Enviar Pitch</p>
+            </div>
+          </div>
+
+          <div class="w-full flex bg-white h-16 justify-start items-center uppercase p-10">
+            <div class="h-[52px] text-lg p-4 flex-col justify-center items-center pb-4 gap-2.5 inline-flex cursor-pointer" :class="{ active: isActive('/profile') }">
+              <router-link to="/profile" class="text-slate-400 font-semibold leading-tight tracking-wide">Perfil</router-link>
+            </div>
+            <div class="h-[52px] text-lg p-4 flex-col justify-center items-center gap-2.5 inline-flex cursor-pointer" :class="{ active: isActive('/vacancies') }">
+              <a href="/vacancies" class="text-slate-400 font-semibold leading-tight tracking-wide">Vacantes</a>
+            </div>
+            <div class="h-[52px] text-lg p-4 flex-col justify-center items-center gap-2.5 inline-flex cursor-pointer">
+              <a href="#" class="text-slate-400 font-semibold leading-tight tracking-wide">Listas</a>
+            </div>
+          </div>
+
+          <div>
+            <router-view />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import CardPage from '@/components/Card/CardPage.vue';
+
+  export default {
+    name: 'companyPage',
+    components: {
+      CardPage,
+    },
+    methods: {
+      isActive(route) {
+        return this.$route.path === route;
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    width: 100%;
+    max-width: 100%;
+  }
+
+  .bg-cover {
+    background-size: cover;
+  }
+
+  .bg-center {
+    background-position: center;
+  }
+
+  .active {
+    --tw-border-opacity: 1;
+    border-color: rgb(219 39 119 / var(--tw-border-opacity));
+    border-bottom-width: 2px;
+    --tw-text-opacity: 1;
+    color: rgb(219 39 119 / var(--tw-text-opacity));
+  }
+</style>
